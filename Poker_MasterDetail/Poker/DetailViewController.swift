6aa06692df70e1197c428267bc5726cd7ad48211
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-04-04 09:52:26 +1000

    Add new project

---
+++ Poker_MasterDetail/Poker/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Poker
+//
+//  Created by Lucas Cullen on 3/4/19.
+//  Copyright © 2019 Bitcoin Brisbane. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
