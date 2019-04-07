d1ac9cda9a9ae136bc3207412f43b90f3ac7e887
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-04-07 15:01:37 +1000

    Add pods and segue

--- Poker_MasterDetail/Poker/Controllers/HomeViewController.swift
+++ Poker_MasterDetail/Poker/Controllers/HomeViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+import SwiftyJSON
+import Alamofire
+
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var labLocation: UIView!
@@ -16,37 +19,40 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     fileprivate let apiURL = "https://dugongsoftware.github.io/GutShotFeed/tournaments.json"
     //private var data: [String] = []
     
-    private var data = [EventDetailsViewModel]()
+    private var eventDetailsList = [EventDetailsViewModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let eventDetailsViewModel = EventDetailsViewModel(name: "Q&A", description: "Monday", start: Date(), buyIn: 100, fee: 10)
         
-        self.data.append(eventDetailsViewModel)
+        self.eventDetailsList.append(eventDetailsViewModel)
         
         tblEvents.delegate = self
         tblEvents.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showDetail" {
+            if let indexPath = tblEvents.indexPathForSelectedRow {
+                //let object = objects[indexPath.row] as! NSDate
+                
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = nil //eventDetailsList
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
     }
-    */
+ 
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.data.count
+        return self.eventDetailsList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
-        let text = data[indexPath.row]._name //2.
+        let text = self.eventDetailsList[indexPath.row]._name //2.
         cell.textLabel?.text = text //3.
         
         return cell //4.
@@ -54,5 +60,82 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
+    }
+}
+
+extension HomeViewController {
+    
+    func requestEventList() {
+        
+        //SwiftSpinner.show("Fetching events")
+        
+        Alamofire.request(apiURL).responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                let count = swiftyJsonVar.count
+                
+                for i in 0...count-1 {
+                    let event = swiftyJsonVar[i]
+                    let eventDictionary = event.dictionaryObject
+                    
+                    //do the rest of the properties from json
+                    let eventname = eventDictionary!["name"] as? String
+                    let buyin = eventDictionary!["buyin"] as? Double
+                    let fee = eventDictionary!["fee"] as? Double
+                    let location = eventDictionary!["location"] as? String
+                    
+                    //let type = eventDictionary!["type"] as? String
+                    //let stack = eventDictionary!["stack"] as? Double
+                    //let levels = eventDictionary!["levels"] as? Double
+                    //let rebuys = eventDictionary!["rebuys"] as? Bool
+                    
+                    let start = eventDictionary!["start"] as? String
+                    
+                    let start_string = start!.components(separatedBy: ":")
+                    let frequency = eventDictionary!["frequency"] as? String
+                    
+                    if (frequency == "weekly") {
+                        
+                        let calendar = Calendar(identifier: .gregorian)
+                        let weekday = eventDictionary!["f_value"] as? Int
+                        let _components = DateComponents(calendar: calendar, weekday: weekday)
+                        
+                        let nextEvent = calendar.nextDate(after: Date(), matching: _components, matchingPolicy: .nextTimePreservingSmallerComponents)
+                        
+                        for i in 0...5 {
+                            var dateComponent = DateComponents()
+                            dateComponent.day = 7 * i
+                            dateComponent.hour = Int(start_string[0])
+                            dateComponent.minute = Int(start_string[1])
+                            
+                            let futureDate = Calendar.current.date(byAdding: dateComponent, to: nextEvent!)
+                            
+                            let eventDetailsViewModel = EventDetailsViewModel(name: eventname!, description: location!, start: futureDate!, buyIn: buyin!, fee: fee!)
+                            
+                            self.eventDetailsList.append(eventDetailsViewModel)
+                        }
+                    }
+                    
+                    if (frequency == "once") {
+                        let formatter = DateFormatter()
+                        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+                        let f_value = eventDictionary!["f_value"] as? String
+                        let _start = formatter.date(from: f_value!)
+                        
+                        let eventDetailsViewModel = EventDetailsViewModel(name: eventname!, description: location!, start: _start!, buyIn: buyin!, fee: fee!)
+                        
+                        self.eventDetailsList.append(eventDetailsViewModel)
+                    }
+                    
+                    self.eventDetailsList = self.eventDetailsList.sorted(by: {$0._start.timeIntervalSince1970 < $1._start.timeIntervalSince1970})
+                    
+                    //let eventModel = EventModel(json: eventDictionary!)
+                    //self.eventList.append(eventModel!)
+                }
+                
+                //self.collectionView.reloadData()
+                //SwiftSpinner.hide()
+            }
+        }
     }
 }
